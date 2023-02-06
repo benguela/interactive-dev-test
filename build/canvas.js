adb4da8eaf0c732a9024e18d840891d813454a22
ahmedtaaw <ahmedtaaw@gmail.com>
2023-02-07 03:55:47 +0400

    circle animation radius

--- build/canvas.js
+++ build/canvas.js
@@ -10,4 +10,21 @@ c2d.fillStyle = 'rgba(255,0,0,0,0.5)';
 c2d.fillRect(100, 100, 100, 100);
 c2d.fillRect(400, 100, 100, 100);
 c2d.fillRect(300, 300, 100, 100);
-https://www.youtube.com/watch?v=yq2au9EfeRQ&list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTPXL&index=3
+let x = 200;
+let y = 200;
+let velocity = 5;
+let circleRadius = 30;
+function animate() {
+    requestAnimationFrame(animate);
+    c2d.clearRect(0, 0, innerWidth, innerHeight);
+    c2d.beginPath();
+    c2d.arc(x, y, circleRadius, 0, Math.PI * 2, false);
+    c2d.strokeStyle = 'blue';
+    c2d.stroke();
+    if (x + circleRadius > innerWidth || x - circleRadius < 0) {
+        velocity = -velocity;
+        //x speed or velocity to ward direction
+    }
+    x += velocity;
+}
+animate();
